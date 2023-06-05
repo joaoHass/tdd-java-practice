27a75d5196073a778e1fed1d20fefa4001995707
joao.hass <joaopedrohass@outlook.com>
2023-06-05 18:01:56 -0300

    test (red): every door should be open after the first pass

--- src/test/java/hass/katas/hundred_doors/HundredDoorsTest.java
+++ src/test/java/hass/katas/hundred_doors/HundredDoorsTest.java
@@ -17,4 +17,14 @@ class HundredDoorsTest {
             assertEquals('#', door);
         }
     }
+
+    @Test
+    @DisplayName("Every door should be open after first pass")
+    void every_door_should_be_open_after_the_first_pass() {
+        HundredDoorsGame game = new HundredDoorsGame();
+        game.pass();
+        for (Character door : game.getDoors()) {
+            assertEquals('@', door);
+        }
+    }
 }
